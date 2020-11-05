f7a9b2ab95517d119ef10d58f4b6069fb4c09618
CongMr <17319131430@163.com>
2020-11-05 20:56:42 +0800

    '添加了一些文件'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 createApp(App).mount('#app')
